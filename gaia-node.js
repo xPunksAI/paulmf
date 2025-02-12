383cf72ada50c5d3de3300b6854cac789290dbf9
Norika Kizawa <norika@0xcerberus.io>
2025-02-12 15:47:10 +0900

    add cutom gaia-node

--- gaia-node.js
+++ gaia-node.js
@@ -1,16 +1,20 @@
 // sample doc: https://docs.gaianet.ai/agent-integrations/intro
 import OpenAI from 'openai';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// my custom node: https://0x970843fcc68194f4645588c6b47fae40e8ac441f.us.gaianet.network/v1
 const gaiaNodeURL = 'https://0x970843fcc68194f4645588c6b47fae40e8ac441f.us.gaianet.network/v1';
 
 const client = new OpenAI({
     baseURL: gaiaNodeURL,
-    apiKey: process.env['GAIA_API_KEY'], // This is the default and can be omitted
+    apiKey: process.env['OPENAI_API_KEY'], // This is the default and can be omitted
   });
   
   async function main() {
     const chatCompletion = await client.chat.completions.create({
       messages: [{ role: 'user', content: 'Say this is a test' }],
-      model: 'gpt-4o',
+      model: 'llama-3-8b-instruct',
     });
     chatCompletion();
   }
